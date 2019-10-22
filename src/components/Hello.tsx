fbc579718ddea75ccfa4b6514a9c56e656eee09a
Jad2Wizard <526654076@qq.com>
2019-10-22 17:00:55 +0800

    test

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import {useState} from 'react'
 
 interface HelloProps {
 	compiler: string
@@ -6,9 +7,13 @@ interface HelloProps {
 }
 
 export const Hello = (props: HelloProps) => {
+	const [count, setCount] = useState(0)
 	return (
-		<h1 style={{width: '100%'}} onClick={() => {}}>
-			Hello from {props.compiler} and {props.framework}!
+		<h1
+			style={{width: '100%'}}
+			onClick={() => setCount(count => count + 1)}
+		>
+			Hello from {props.compiler} and {props.framework}!{count}
 		</h1>
 	)
 }
