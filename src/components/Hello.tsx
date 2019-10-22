8ea9048e244a1ae5f7c7ad53caa5c8ccbea8335c
Jad2Wizard <526654076@qq.com>
2019-10-22 16:40:47 +0800

    test

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react'
 
 interface HelloProps {
-    compiler: string,
-    framework: string
+	compiler: string
+	framework: string
 }
 
 export const Hello = (props: HelloProps) => {
-    return <h1>Hello from {props.compiler} and {props.framework}!</h1>
+	return (
+		<h1 style={{width: '100%'}} onClick={() => {}}>
+			Hello from {props.compiler} and {props.framework}!
+		</h1>
+	)
 }
