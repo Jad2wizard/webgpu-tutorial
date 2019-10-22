3db9f4d5dff45ea277ed86741329725ddf76f5bc
Jad2Wizard <526654076@qq.com>
2019-10-22 16:06:59 +0800

    test

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -7,6 +7,5 @@ interface HelloProps {
 
 export const Hello = (props: HelloProps) => {
     console.log('Bingo')
-    console.log()
     return <h1>Hola from {props.compiler} and {props.framework}!</h1>
 }
