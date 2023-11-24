9ea092d871cebe1a58e0756f2d75d21db7a8d76a
Jad2Wizard <526654076@qq.com>
2023-11-24 16:07:18 +0800

    初始化 WebGPU

--- src/webgpu/test.ts
+++ src/webgpu/test.ts
@@ -1,35 +1,4 @@
-const genPositionAndIndex = (
-	outerRadius: number,
-	innerRadius: number,
-	numSubDivisions: number
-) => {
-	// const position: number[] = [0.0, 0.5, 0.5, 0.5, 0.5, 0.0, 0.0, 0.0]
-	// const index: number[] = [0, 1, 2, 0, 2, 3]
-	const position: number[] = []
-	const index: number[] = []
-	const angleStride = (Math.PI * 2) / numSubDivisions
-	let angle = 0
-	for (let i = 0; i < numSubDivisions; ++i) {
-		position.push(
-			Math.sin(angle) * outerRadius,
-			Math.cos(angle) * outerRadius,
-			angle / (2 * Math.PI),
-			Math.sin(angle) * innerRadius,
-			Math.cos(angle) * innerRadius,
-			angle / (2 * Math.PI)
-		)
-		const ni = i === numSubDivisions - 1 ? 0 : i + 1
-		index.push(i * 2, i * 2 + 1, ni * 2, i * 2 + 1, ni * 2 + 1, ni * 2)
-		angle += angleStride
-	}
-	return {
-		vertexData: new Float32Array(position),
-		indexData: new Uint32Array(index),
-		numVertices: index.length
-	}
-}
-
-async function main(canvas: any) {
+async function main(canvas: HTMLCanvasElement) {
 	const adapter = await navigator.gpu?.requestAdapter()
 	const device = await adapter?.requestDevice()
 	if (!device) {
@@ -39,169 +8,33 @@ async function main(canvas: any) {
 
 	// Get a WebGPU context from the canvas and configure it
 	const context = canvas.getContext('webgpu')
-	const presentationFormat = navigator.gpu.getPreferredCanvasFormat()
+	if (!context) return
+	const canvasFormat = navigator.gpu.getPreferredCanvasFormat()
+	//配置 canvas 作为device 的输出纹理。device 并不与某个 canvas 绑定。
+	//一个 device 可以输出渲染到多个画布上
 	context.configure({
 		device,
-		format: presentationFormat,
-		alphaMode: 'premultiplied'
+		format: canvasFormat //纹理格式
 	})
 
-	const module = device.createShaderModule({
-		label: 'triangle shaders with uniforms',
-		code: `
-		struct VSOutput { //定义顶点着色器输出，color 作为 inter stage 变量，将顶点颜色通过 location(0) 传给片元着色器
-			@builtin(position) position: vec4f,
-			@location(0) angle: f32
-		}
-		struct OurStruct {
-			scale: f32,
-			color: vec3f
-		}
-
-		//顶点 attribute 变量结构体，此处用于定义顶点着色器的输入参数
-		struct Vertex {
-			@location(0) position: vec2f, //position attribute，此处的 location(0)和 VSOutput.color 的 location(0) 是不一样的。一个是顶点着色器 attribute 的通道，一个是顶点着色器到片元着色器的 inter-stage 变量通道
-			@location(1) angle: f32,
-			@builtin(vertex_index) vertexIndex : u32
-		}
-
-		@group(0) @binding(0) var<uniform> ourStruct: OurStruct;
-
-		@vertex fn vs(vert: Vertex) -> VSOutput {
-			var output: VSOutput;
-			var pos = vert.position;
-			let color = vec4f(vert.angle, 0, 0, 1);
-			output.position = vec4f(pos * vec2f(ourStruct.scale), 0, 1);
-			output.angle = vert.angle;
-			return output;
-		}
-  
-		@fragment fn fs(@location(0) angle: f32) -> @location(0) vec4f {
-			return vec4f(angle, 0, 0, angle);
-		}
-	  `
-	})
-
-	const {vertexData, indexData, numVertices} = genPositionAndIndex(
-		0.45,
-		0.3,
-		256
-	)
-	console.log(vertexData, indexData, numVertices)
-
-	//定义 vertext buffer，该 buffer 存放了position 和 color 两个 attribute
-	const vertexBuffer = device.createBuffer({
-		label: 'vertex buffer vertices',
-		size: vertexData.byteLength,
-		// eslint-disable-next-line no-undef
-		usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
-	})
-	device.queue.writeBuffer(vertexBuffer, 0, vertexData)
-
-	const indexBuffer = device.createBuffer({
-		label: 'index buffer',
-		size: indexData.byteLength,
-		// eslint-disable-next-line no-undef
-		usage: GPUBufferUsage.INDEX | GPUBufferUsage.COPY_DST
-	})
-	device.queue.writeBuffer(indexBuffer, 0, indexData)
-
-	const pipeline = device.createRenderPipeline({
-		label: 'our hardcoded red triangle pipeline',
-		layout: 'auto',
-		vertex: {
-			module,
-			entryPoint: 'vs',
-			buffers: [
-				{
-					//描述第一个 buffer layout，此处并不创建实际的 buffer
-					arrayStride: 3 * 4, //  等于一个顶点放在 vertexBuff 中的所有 attribute 的数据长度。position attribute 为 vec2f 类型，每个 position 在 buffer 中的长度位2 * 4字节，color 为 3 * 4字节
-					attributes: [
-						{
-							//position attribute
-							shaderLocation: 0, //每个 attribute 点一个唯一索引值，0-15。shaderLocation 与该 attribute 在顶点着色器中定义的该 attribute 变量的@location 必须保持一致
-							offset: 0, //表示该 attribute 在 buffer 中的偏移字节数
-							format: 'float32x2' //attribute 的数据类型
-						},
-						{
-							//angle attribute
-							shaderLocation: 1, //每个 attribute 点一个唯一索引值，0-15。shaderLocation 与该 attribute 在顶点着色器中定义的该 attribute 变量的@location 必须保持一致
-							offset: 2 * 4, //表示该 attribute 在 buffer 中的偏移字节数
-							format: 'float32' //attribute 的数据类型
-						}
-					]
-				}
-			]
-		},
-		fragment: {
-			module,
-			entryPoint: 'fs',
-			targets: [{format: presentationFormat}]
-		}
-	})
-
-	//color 16字节 + scale 8字节 + offset 8字节，一共需要一个32字节长度的 buffer 来存放 uniform
-	const uniformBufferSize = 4 + 12 + 12 + 4
-	//uniformBuffer 是一个 GPUBuffer，用于存放color,scale,offset的值
-	const uniformBuffer = device.createBuffer({
-		label: 'uniforms for triangle',
-		size: uniformBufferSize,
-		// eslint-disable-next-line no-undef
-		usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
-	})
-	//uniformValue 是一个 typedArray，用于写入uniform
-	const uniformValue = new Float32Array(uniformBufferSize / 4)
-	//根据 color,scale,offset 在 OurStruct 中的顺序， 将三个值通过uniformValue 写入 buffer
-	uniformValue.set([1], 0) //写入scale， 在 typedArray 中的偏移量为color 的长度，即4
-	uniformValue.set([0, 0, 0], 4) //写入color， 在 typedArray 中的偏移量为0
-
-	//创建 bindGroup 对象，将 buffer 绑定到@binding(0)位置
-	const bindGroup = device.createBindGroup({
-		label: 'triangle bind group',
-		layout: pipeline.getBindGroupLayout(0),
-		entries: [{binding: 0, resource: {buffer: uniformBuffer}}]
-	})
-
-	const renderPassDescriptor = {
-		label: 'our basic canvas renderPass',
+	//GPUCommandEncoder，开始或结束渲染通道。通过 finish 接口创建命令缓冲区 command buffer
+	const encoder = device.createCommandEncoder()
+	//GPURenderPassEncoder，定义一个渲染通道
+	const pass = encoder.beginRenderPass({
 		colorAttachments: [
 			{
-				// view: <- to be filled out when we render
-				clearValue: [0, 0, 0, 1],
-				loadOp: 'clear',
-				storeOp: 'store'
+				view: context.getCurrentTexture().createView(), //渲染通道到输出纹理
+				loadOp: 'clear', //渲染通道开启前用clearValue 清空输出纹理，即 canvas
+				storeOp: 'store', //渲染通道完成以后，将绘制结果保存到输出纹理
+				clearValue: {r: 0, g: 0.235, b: 0.3804, a: 0.1} //透明度设置无效
 			}
 		]
-	}
+	})
+	pass.end()
 
-	function render() {
-		if (!device) return
-
-		//将 uniform 的值从  js 的 typedArray 读入到 GPUBuffer 中
-		device.queue.writeBuffer(uniformBuffer, 0, uniformValue)
-		// Get the current texture from the canvas context and
-		// set it as the texture to render to.
-		renderPassDescriptor.colorAttachments[0].view = context
-			.getCurrentTexture()
-			.createView()
-
-		// make a command encoder to start encoding commands
-		const encoder = device.createCommandEncoder({label: 'our encoder'})
-
-		// make a render pass encoder to encode render specific commands
-		const pass = encoder.beginRenderPass(renderPassDescriptor)
-		pass.setPipeline(pipeline)
-		pass.setVertexBuffer(0, vertexBuffer)
-		pass.setIndexBuffer(indexBuffer, 'uint32')
-		pass.setBindGroup(0, bindGroup)
-		pass.drawIndexed(numVertices) // call our vertex shader 3 times.
-		pass.end()
-
-		const commandBuffer = encoder.finish()
-		device.queue.submit([commandBuffer])
-	}
-
-	render()
+	//commandBuffer 提交后就无法再次使用
+	const commandBuffer = encoder.finish()
+	device.queue.submit([commandBuffer])
 }
 
 export default main
